d670c2fbd7ab29ab0ff38e9396cfbb7bd3343d40
tt <tamara.trefilova@gmail.com>
2019-06-07 13:22:33 -0700

    added new function for calculating the result

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,25 @@ var bookAnswer;
 var compAnswer;
 var petAnswer;
 
+var getAnswer = function(age, answer1, answer2, answer3, answer4, answer5) {
+  if (age > 70) {
+    return "<div> <a href=\"http://www.codecademy.com\">Please check Code academy!</a> </div>";
+  } else if (age < 12) {
+    return "<div> <a href=\"http://www.tynker.com\">Please check this! It is going to be fun! </a> </div>";
+  } else if ((answer1 === "option1" || answer1 === "option2") && (answer2 === "option1" || answer2 === "option2") && (answer3 === "option1" || answer3 === "option2") && (answer4 === "option1" || answer4 === "option2") && (answer5 === "option1" || answer5 === "option2")) {
+    return "You are Java person!";
+  } else if ((answer1 === "option3" || answer1 === "option2") && (answer2 === "option3" || answer2 === "option2") && (answer3 === "option3" || answer3 === "option2") && (answer4 === "option3" || answer4 === "option2") && (answer5 === "option3" || answer5 === "option2")) {
+    return " You are JavaScript person!";
+  } else if ((answer1 === "option3" || answer1 === "option1") && (answer2 === "option1" || answer2 === "option3") && (answer3 === "option1" || answer3 === "option3") && (answer4 === "option1" || answer4 === "option3") && (answer5 === "option1" || answer5 === "option3")) {
+    return "You are C# person!";
+  } else if ((answer1 === "option1" || answer1 === "option2") && (answer2 === "option4" || answer2 === "option2") && (answer3 === "option3" || answer3 === "option2") && (answer4 === "option4" || answer4 === "option2") && (answer5 === "option4" || answer5 === "option2")) {
+    return "You are Python person!";
+  } else if ((answer1 === "option3" || answer1 === "option2") && (answer2 === "option1" || answer2 === "option2") && (answer3 === "option3" || answer3 === "option1") && (answer4 === "option4" || answer4 === "option1") && (answer5 === "option1" || answer5 === "option4")) {
+    return "You are Ruby person!";
+  } else {
+    return "Are you sure that you want to learn how to code?)))"
+  }
+};
 
 $(document).ready(function() {
   $("form").submit(function() {
@@ -17,22 +36,7 @@ $(document).ready(function() {
     bookAnswer = $("input:radio[name=bed]:checked").val();
     compAnswer = $("input:radio[name=bed]:checked").val();
     petAnswer = $("input:radio[name=bed]:checked").val();
-    var result;
-    // alert(bedAnswer + instrumentAnswer+ bookAnswer +compAnswer+petAnswer);
-    if ( (bedAnswer==="option1" || bedAnswer==="option2") && (bookAnswer==="option1" || bookAnswer==="option2") && (compAnswer==="option1" || compAnswer==="option2") && (instrumentAnswer==="option1" || instrumentAnswer==="option2") && (petAnswer==="option1" || petAnswer==="option2")){
-      result = "You are Java person!";
-    } else if ( (bedAnswer==="option3" || bedAnswer==="option2") && (bookAnswer==="option3" || bookAnswer==="option2") && (compAnswer==="option3" || compAnswer==="option2") && (instrumentAnswer==="option3" || instrumentAnswer==="option2") && (petAnswer==="option3" || petAnswer==="option2")){
-        result = " You are JavaScript person!";
-    } else if ( (bedAnswer==="option3" || bedAnswer==="option1") && (bookAnswer==="option1" || bookAnswer==="option3") && (compAnswer==="option1" || compAnswer==="option3") && (instrumentAnswer==="option1" || instrumentAnswer==="option3") && (petAnswer==="option1" || petAnswer==="option3")){
-        result = "You are C# person!";
-    } else if ( (bedAnswer==="option1" || bedAnswer==="option2") && (bookAnswer==="option4" || bookAnswer==="option2") && (compAnswer==="option3" || compAnswer==="option2") && (instrumentAnswer==="option4" || instrumentAnswer==="option2") && (petAnswer==="option4" || petAnswer==="option2")){
-        result = "You are Python person!";
-    } else if ( (bedAnswer==="option3" || bedAnswer==="option2") && (bookAnswer==="option1" || bookAnswer==="option2") && (compAnswer==="option3" || compAnswer==="option1") && (instrumentAnswer==="option4" || instrumentAnswer==="option1") && (petAnswer==="option1" || petAnswer==="option4")){
-        result = "You are Ruby person!";
-    } else {
-      result = "Are you sure that you want to learn how to code?)))"
-    }
-
+    var result = getAnswer(age, bedAnswer, instrumentAnswer, bookAnswer, compAnswer, petAnswer );
     $(".nameOutput").append(name);
     $(".languageOutput").append(result);
     $(".result").show();
