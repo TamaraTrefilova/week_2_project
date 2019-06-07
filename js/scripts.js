834a3a4eb0f5b47eed7aa4431902000b31a41d25
tt <tamara.trefilova@gmail.com>
2019-06-07 16:31:36 -0700

    last commit

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,15 @@ var getAnswer = function(age, answer1, answer2, answer3, answer4, answer5) {
     return "<div> <a href=\"http://www.codecademy.com\">Please check Code academy!</a> </div>";
   } else if (age < 12) {
     return "<div> <a href=\"http://www.tynker.com\">Please check this! It is going to be fun! </a> </div>";
-  } else if ((answer1 === "option1" || answer1 === "option2") && (answer2 === "option1" || answer2 === "option2") && (answer3 === "option1" || answer3 === "option2") && (answer4 === "option1" || answer4 === "option2") && (answer5 === "option1" || answer5 === "option2")) {
+  } else if ((answer1 === "option1") && (answer2 === "option1") && (answer3 === "option1" ) && (answer4 === "option2") && (answer5 === "option1" || answer5 === "option2")) {
     return "You are Java person!";
-  } else if ((answer1 === "option3" || answer1 === "option2") && (answer2 === "option3" || answer2 === "option2") && (answer3 === "option3" || answer3 === "option2") && (answer4 === "option3" || answer4 === "option2") && (answer5 === "option3" || answer5 === "option2")) {
+  } else if (( answer1 === "option2") && (answer2 === "option3" || answer2 === "option2") && (answer3 === "option3" || answer3 === "option2") && (answer4 === "option3" || answer4 === "option2") && (answer5 === "option3" || answer5 === "option2")) {
     return " You are JavaScript person!";
   } else if ((answer1 === "option3" || answer1 === "option1") && (answer2 === "option1" || answer2 === "option3") && (answer3 === "option1" || answer3 === "option3") && (answer4 === "option1" || answer4 === "option3") && (answer5 === "option1" || answer5 === "option3")) {
     return "You are C# person!";
-  } else if ((answer1 === "option1" || answer1 === "option2") && (answer2 === "option4" || answer2 === "option2") && (answer3 === "option3" || answer3 === "option2") && (answer4 === "option4" || answer4 === "option2") && (answer5 === "option4" || answer5 === "option2")) {
+  } else if ((answer1 === "option1" || answer1 === "option2") && (answer2 === "option4") && (answer3 === "option3" || answer3 === "option2") && (answer4 === "option4" || answer4 === "option2") && (answer5 === "option4" || answer5 === "option2")) {
     return "You are Python person!";
-  } else if ((answer1 === "option3" || answer1 === "option2") && (answer2 === "option1" || answer2 === "option2") && (answer3 === "option3" || answer3 === "option1") && (answer4 === "option4" || answer4 === "option1") && (answer5 === "option1" || answer5 === "option4")) {
+  } else if ((answer1 === "option3") && (answer2 === "option1") && (answer3 === "option3" || answer3 === "option1") && (answer4 === "option4" || answer4 === "option1") &&  answer5 === "option4")) {
     return "You are Ruby person!";
   } else {
     return "Are you sure that you want to learn how to code?)))"
@@ -40,6 +40,6 @@ $(document).ready(function() {
     $(".nameOutput").append(name);
     $(".languageOutput").append(result);
     $(".modal").show();
-    $(".modal").modal("hide"); 
+    $(".modal").modal("hide");
   });
 });
